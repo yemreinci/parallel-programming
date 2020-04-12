a04cbff073f706e729a30d68ffbf5ae164e1b0ba
Inci Yunus <inciy1@albatrossi.org.aalto.fi>
2020-04-13 02:03:40 +0300

    MF1 - implement mf

--- mf1/mf.cc
+++ mf1/mf.cc
@@ -1,5 +1,31 @@
 #include "mf.h"
+#include <algorithm>
+
+using namespace std;
 
 void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
-    // FIXME
+    float* window = new float[(2*hx + 1) * (2*hy + 1)];
+
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            
+            int wi = 0;
+            for (int j = max(0, y-hy); j <= min(y+hy, ny-1); j++) { 
+                for (int i = max(0, x-hx); i <= min(x+hx, nx-1); i++) {
+                     window[wi++] = in[i + nx*j];
+                }
+            }
+            
+            nth_element(window, window + (wi/2), window + wi);
+            if (wi % 2) {
+                out[x + nx*y] = window[wi/2];
+            }
+            else {
+                nth_element(window, window + (wi/2 - 1), window + wi);
+                out[x + nx*y] = (window[wi/2] + window[wi/2 - 1]) / 2;
+            }
+        }
+    }
+
+    delete[] window;
 }
