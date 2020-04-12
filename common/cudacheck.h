7050207d193d35606ec678c1002bec59403e4187
Jukka Suomela <jukka.suomela@iki.fi>
2020-04-12 12:56:54 +0300

    PPC 2020 code templates.

---
+++ common/cudacheck.h
@@ -0,0 +1,18 @@
+#ifndef CUDACHECK_H
+#define CUDACHECK_H
+
+#include <cstdlib>
+#include <iostream>
+#include <cuda_runtime.h>
+
+static inline void check(cudaError_t err, const char* context) {
+    if (err != cudaSuccess) {
+        std::cerr << "CUDA error: " << context << ": "
+            << cudaGetErrorString(err) << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK(x) check(x, #x)
+
+#endif
