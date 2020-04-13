5557531844646df714f02c538485a1bb24073d31
Inci Yunus <inciy1@albatrossi.org.aalto.fi>
2020-04-14 00:07:46 +0300

    CP2b - implement correlate

--- cp2b/cp.cc
+++ cp2b/cp.cc
@@ -1,5 +1,41 @@
 #include "cp.h"
+#include <cmath>
+#include <iostream>
 
 void correlate(int ny, int nx, const float* data, float* result) {
-    // TODO
+    double* normal = new double[nx * ny];
+
+    for (int y = 0; y < ny; y++) {
+        double mean = 0, std = 0;
+
+        for (int x = 0; x < nx; x++) {
+            mean += data[x + y*nx] / nx;
+        }
+
+        for (int x = 0; x < nx; x++) {
+            double diff = data[x + y*nx] - mean;
+            std += diff * diff;
+        }
+
+        std = sqrt(std);
+
+        for (int x = 0; x < nx; x++) {
+            normal[x + y*nx] = (data[x + y*nx] - mean) / std;
+        }
+    }
+    
+    #pragma omp parallel for schedule(static, 1)
+    for (int j = 0; j < ny; j++) {
+        for (int i = j; i < ny; i++) {
+            double t = 0;
+
+            for (int k = 0; k < nx; k++) {
+                t += normal[k + i*nx] * normal[k + j*nx];
+            }
+
+            result[i + j*ny] = t;
+        }
+    }
+
+    delete[] normal;
 }
