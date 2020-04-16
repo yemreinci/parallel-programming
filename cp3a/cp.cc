6f1b5207bb94b47a774a8251e66aa088eb46e900
Inci Yunus <inciy1@smaragdi.org.aalto.fi>
2020-04-16 20:27:08 +0300

    CP3a - 1.6s

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -1,6 +1,7 @@
 #include "cp.h"
 #include <cmath>
 #include <new>
+#include <x86intrin.h>
 
 typedef double double4_t __attribute__ ((vector_size (4 * sizeof(double))));
 
@@ -12,75 +13,85 @@ static double4_t* double4_alloc(std::size_t n) {
     return (double4_t*)tmp;
 }
 
+static inline double4_t swap1(double4_t x) { return _mm256_permute_pd(x, 5); }
+static inline double4_t swap2(double4_t x) { return _mm256_permute2f128_pd(x, x, 1); }
+
 void correlate(int ny, int nx, const float* data, float* result) {
-    constexpr int nb = 4;
-    int na = (nx + nb - 1) / nb;
+    constexpr int nb = 4; 
+    constexpr int nd = 2;
 
-    constexpr int nd = 6;
-    int nc = (ny + nd - 1) / nd;
-    int ncd = nc * nd;
+    int na = ((ny + nb - 1) / nb + nd - 1) / nd * nd;
 
-    double4_t* normal = double4_alloc(ncd * na);
+    double4_t* normal = double4_alloc(na * nx);
 
     #pragma omp parallel for schedule(static, 1)
-    for (int y = 0; y < ny; y++) {
-        double mean = 0, std = 0;
+    for (int ja = 0; ja < na; ja++) {
+        for (int jb = 0; jb < nb; jb++) {
+            int j = ja*nb + jb;
 
-        for (int x = 0; x < nx; x++) {
-            mean += data[x + y*nx] / nx;
-        }
+            if (j < ny) {
+                double mean = 0, std = 0;
 
-        for (int x = 0; x < nx; x++) {
-            double diff = data[x + y*nx] - mean;
-            std += diff * diff;
-        }
-
-        std = sqrt(std);
-
-        for (int x = 0; x < na; x++) {
-            for (int z = 0; z < nb; z++) {
-                if (z + x*nb < nx)
-                    normal[x + y*na][z] = data[z + x*nb + y*nx];
-                else
-                    normal[x + y*na][z] = mean;
-            }
-
-            normal[x + y*na] -= mean;
-            normal[x + y*na] /= std;
-        }
-
-    }
-
-    for (int y = ny; y < ncd; y++) {
-        for (int x = 0; x < na; x++) {
-            for (int z = 0; z < nb; z++) {
-                normal[x + y*na][z] = 0;
-            }
-        }
-    }
-
-    #pragma omp parallel for schedule(static, 1)
-    for (int jc = 0; jc < nc; jc++) {
-        for (int ic = jc; ic < nc; ic++) {
-            double4_t t[nd][nd] = {};
-            
-            for (int k = 0; k < na; k++) {
-
-                for (int jd = 0; jd < nd; jd++) {
-                    for (int id = 0; id < nd; id++) {
-                        t[jd][id] += normal[k + (jc*nd + jd)*na] * normal[k + (ic*nd + id)*na];
-                    }
+                for (int i = 0; i < nx; i++) {
+                    mean += (double) data[i + j*nx] / nx;
                 }
 
+                for (int i = 0; i < nx; i++) {
+                    double diff = data[i + j*nx] - mean;
+                    std += diff * diff;
+                }
+
+                std = sqrt(std);
+
+                for (int i = 0; i < nx; i++) {
+                    normal[i + ja*nx][jb] = (data[i + j*nx] - mean) / std;
+                }
             }
-            
-            for (int jd = 0; jd < nd; jd++) {
-                int j = jc * nd + jd;
-                for (int id = 0; id < nd; id++) {
-                    int i = ic * nd + id;
-                    if (i >= j && i < ny && j < ny) {
-                        result[i + j*ny] = t[jd][id][0] + t[jd][id][1] + t[jd][id][2] + t[jd][id][3];
+            else {
+                for (int i = 0; i < nx; i++) {
+                    normal[i + ja*nx][jb] = 0;
+                }
+            }
+        }
+
+    }
+   
+    #pragma omp parallel for schedule(static, 1)
+    for (int ja = 0; ja < na/nd; ja++) {
+        for (int ia = ja; ia < na/nd; ia++) {
+            double4_t t[nd][nd][nb] = {};
+
+            for (int k = 0; k < nx; k++) {
+                for (int jd = 0; jd < nd; jd++) {
+                    for (int id = 0; id < nd; id++) {
+                        //constexpr int PF = 4;
+                        //__builtin_prefetch(&normal[ia*nx + k + PF]);
+                        //__builtin_prefetch(&normal[ja*nx + k + PF]);
+
+                        double4_t a00 = normal[k + (ia*nd + jd)*nx];
+                        double4_t b00 = normal[k + (ja*nd + id)*nx];
+                        double4_t a01 = swap1(a00);
+                        double4_t b10 = swap2(b00);
+
+                        t[jd][id][0] += a00 * b00;
+                        t[jd][id][1] += a01 * b00;
+                        t[jd][id][2] += a00 * b10;
+                        t[jd][id][3] += a01 * b10;
                     }
+                }
+            }
+
+
+            for (int jd = 0; jd < nd; jd++) {
+                for (int id = 0; id < nd; id++) {
+                    for (int k = 0; k < nb; k++) {
+                        for (int r = 0; r < nb; r++) {
+                            int j = ((ja*nd + id)*nb + (k ^ (r & 2)));
+                            int i = ((ia*nd + jd)*nb + (k ^ (r & 1)));
+                            if (j < ny && i < ny && i >= j)
+                                result[i + j*ny] = t[jd][id][r][k];
+                        }
+                    } 
                 }
             }
 
